e5dc1f897ba5ce60a9f6958a029759346c36f5b9
edwinfisher <fisher.iserlohn@gmail.com>
2019-06-30 12:09:12 +0800

    Applied Data Science Captone

---
+++ Applied Data Science Capstone.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
